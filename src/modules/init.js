5edaeac2c6c7ddc0f184e5fcd2028073dcf0522a
bofang <bofang@sohu-inc.com>
2015-10-23 00:17:21 +0800

    update

--- src/modules/init.js
+++ src/modules/init.js
@@ -1,7 +1,8 @@
 define(function(require, exports, module) {
     var $ = require('../lib/jquery-2.1.4.min.js');
+    var $$Stroage = require('../lib/localforage.nopromises.min.js');
     var loading = require('common/loading.js');
-
+    console.log($$Stroage);
     if ($('#SOHUCS').length) {
         // 如果有接入畅言得话
         if ($('#cy-cbox-wrapper').length) {
